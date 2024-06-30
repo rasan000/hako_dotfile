574750bba113673f7e780b376a5b8cdb953ac149
rasan000 <rasan000@gmail.com>
2024-07-01 03:16:34 +0900

    wsl用のセットアップファイル追加; ;

---
+++ install.sh
@@ -0,0 +1,37 @@
+-- インストール時に実行する
+sudo apt update
+sudo apt upgrade
+
+
+-- pip
+
+-- neovim
+
+-- lazygit
+
+-- terraform 
+https://developer.hashicorp.com/terraform/tutorials/aws-get-started/install-cli
+
+-- java and setting javahome 
+
+
+-- kotlin
+
+
+
+-- windows path(code & docker)
+
+
+-- awscli sam cdk
+
+-- azure
+
+
+-- fish
+
+-- nvm 
+
+-- configfile fish ssh tmux neovim 
+
+
+--
