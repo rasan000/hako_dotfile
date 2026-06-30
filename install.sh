4973a148201b5776741ff8fb9775cb499c63a7a0
rasan000 <rasan000@gmail.com>
2026-06-30 18:16:08 +0900

    add docker

--- install.sh
+++ install.sh
@@ -15,7 +15,23 @@ sudo apt install -y ripgrep
 sudo apt remove -y vim
 sudo apt install -y vim-gtk3
 
-# Install
+# Install docker
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Install k8s
 
 # Install zsh and set as default shell
 sudo apt install -y zsh
